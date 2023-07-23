7ef079d879976f2bbd64c9831dc549df6e9f0d5a
XxP228 <kamochnok.2548@gmail.com>
2023-07-23 22:39:40 +0700

    hw4-4

--- Homework/HW04/hw04-4-triangle.cpp
+++ Homework/HW04/hw04-4-triangle.cpp
@@ -13,3 +13,13 @@
     Output:
         Triagle Area : 7.5
 */
+#include <stdio.h>
+int main() {
+    float H = 0, W = 0;
+    printf("Please Enter Hight : ");
+    scanf("%f", &H);
+    printf("Please Enter Wight : ");
+    scanf("%f",&W);
+    printf("Triagle Area : %0.1f", (H * W)/2);
+    return 0 ;
+}
